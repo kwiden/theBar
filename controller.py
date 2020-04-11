694e2417c9a91572cc394e8294510b91ffa11b88
kwiden <knwiden@gmail.com>
2020-04-11 15:16:39 -0700

    add functionality that clicking a drink name will move to a new page with ingredient list

--- controller.py
+++ controller.py
@@ -1,11 +1,13 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import (
     NumericProperty, BooleanProperty, ReferenceListProperty, ObjectProperty, StringProperty
 )
 
 from random import randint
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.slider import Slider
@@ -24,6 +26,21 @@ class Command:
 class Controller(FloatLayout):
     label_wid = ObjectProperty()
     info = StringProperty()
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.read_xml()
+        box = BoxLayout(orientation='horizontal', spacing=20, pos=(0,550))
+        i = 0;
+        callbacks = []
+        
+        for item in self.list:
+            btn = Button(text=item.name, on_press=Command(self.do_action, i), size_hint=(.1,.1))
+            i += 1
+            box.add_widget(btn)
+        
+        self.add_widget(box)
+    
         
     def update_item(self):
         this_drink = self.list[self.current_item]
@@ -59,35 +76,70 @@ class Controller(FloatLayout):
 
         
     def do_action(self, this_drink_num):
-        print(this_drink_num[0])
         this_drink = self.list[this_drink_num[0]]
-        print(this_drink.name)
-        for item in this_drink.list:
-            print(item, this_drink.list[item])
         self.current_item = this_drink_num[0]
         self.update_item()
+        bar_app.screen_manager.current = this_drink.name
+        
         #self.label_wid.text = 'My label after button press'
         #self.info = 'New info text'
+        
+class Drink_Page(FloatLayout):
+    
+    def __init__(self, name, ingr_list, **kwargs):
+        super().__init__(**kwargs)
+        
+        la = Label(text=name)
+        
+        box = BoxLayout(orientation='vertical', spacing=20)
+        box.add_widget(la)
+        
+        for item in ingr_list:
+            print(item, ingr_list[item])
+            lab = Label(text=str(item))
+            box.add_widget(lab)
+            #btn = Button(text=item.name, on_press=Command(cont.do_action, i), size_hint=(.1,.1))
+            #i += 1
+            #box.add_widget(btn)
+
+        sbtn = Button(text='Back to Main',on_press = self.go_to_main, size_hint=(.1,.1))
+        self.add_widget(box)
+        self.add_widget(sbtn)
+        
+    def go_to_main(self,instance):
+        bar_app.screen_manager.current = 'Main'
         
 class ControllerApp(App):
 
     def build(self):
-        cont = Controller(info='Hello world')
-        cont.read_xml()
-        box = BoxLayout(orientation='horizontal', spacing=20, pos=(0,550))
-        i = 0;
-        callbacks = []
+        # We are going to use screen manager, so we can add multiple screens
+        # and switch between them
+        self.screen_manager = ScreenManager()
+
+        # Initial, Main screen (we use passed in name to activate screen)
+        # First create a page, then a new screen, add page to screen and screen to screen manager
+        self.main_menu = Controller()
+        screen = Screen(name='Main')
+        screen.add_widget(self.main_menu)
+        self.screen_manager.add_widget(screen)
         
-        for item in cont.list:
-            print("_ _ " + item.name)
-            btn = Button(text=item.name, on_press=Command(cont.do_action, i), size_hint=(.1,.1))
-            i += 1
-            box.add_widget(btn)
+        # Drink pages
+        for drink in self.main_menu.list:
+            self.drinks = Drink_Page(drink.name,drink.list)
+            screen = Screen(name=drink.name)
+            screen.add_widget(self.drinks)
+            self.screen_manager.add_widget(screen)
+
         
-        cont.add_widget(box)
-        return cont
+        
+        #screen = Screen(name='Settings')
+        #screen.add_widget(self.info_page)
+        #self.screen_manager.add_widget(screen)
+
+        return self.screen_manager
+        
 
 
 if __name__ == '__main__':
-    
-    ControllerApp().run()
+    bar_app = ControllerApp()
+    bar_app.run()
